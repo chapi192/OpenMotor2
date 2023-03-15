8c8b7bbaa4fbb6bade6288d131973097fe225b97
Elijah Roell <roell030@umn.edu>
2023-03-15 11:03:29 -0500

    Added header files for all types of controller.

---
+++ src/Controllers.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "ButtonController.hpp"
+#include "CheckBoxController.hpp"
+#include "SeparatorLineController.hpp"
+#include "TabContainerController.hpp"
+#include "TextInputController.hpp"
