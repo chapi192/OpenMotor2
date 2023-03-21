0428460e4c84ed9cc3ec8e57481d188d60de02c1
Elijah Roell <roell030@umn.edu>
2023-03-21 15:29:49 -0500

    addButton adds the button to m_buttons.

--- src/ButtonController.cpp
+++ src/ButtonController.cpp
@@ -6,6 +6,9 @@ void ButtonController::addButton(sf::Vector2f location, sf::Vector2f dimensions,
 	newButton->setPosition(location.x, location.y);
 	newButton->setSize(dimensions.x, dimensions.y);
 	newButton->setText(text);
+	newButton->setVisible(true);
+
+	m_buttons.emplace_back(newButton);
 }
 
 std::vector<tgui::Button::Ptr> ButtonController::getButtons() { return m_buttons; }
