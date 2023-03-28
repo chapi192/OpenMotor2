bb5002132e1908c1b164acb473a79884da6becb3
Elijah Roell <roell030@umn.edu>
2023-03-28 14:59:32 -0500

    Removed all methods corresponding to size and position. Removed switch statment in draw. implemented update and updateGrain. Removed default constructor.

--- src/GrainGraph.cpp
+++ src/GrainGraph.cpp
@@ -1,67 +1,46 @@
 #include "GrainGraph.hpp"
 #include <cmath>
 
-const float PI = 3.14159265
+const float PI = 3.14159265;
 
-Grain grain;
+GrainGraph::GrainGraph(tgui::Container::Ptr container, sf::Vector2f posInContainer, float length) :
+	m_plot(container->getPosition(), m_grain),
+	m_grainCanvas( tgui::CanvasSFML::create() )
+{
+	container->add(m_grainCanvas);
 
-GrainGraph::GrainGraph();
-GrainGraph::GrainGraph(sf::Vector2f position, sf::Vector2f size) { center = { m_position.x + m_size.x / 2, m_position.y + m_size.y / 2 }; }
+	m_grainCanvas->setPosition(container->getPosition());
+	sf::Vector2f containerSize = container->getSize();
+	//float smallestLength = containerSize.x < containerSize.y ? containerSize.x : containerSize.y;
+	//m_grainCanvas->setSize(smallestLength, smallestLength);
+}
 
-GrainGraph::~GrainGraph() = default;
+//GrainGraph::~GrainGraph() = default;
 
 void GrainGraph::draw()
 {
-	if (m_visable) {
-		int numSegments = ( (2.f / 3.f * m_size.x) * PI / m_penSize ); 
-		switch (grain.getGrainGeometry()) {
-		case (TUBE):
-			for (numSegments) {
-				sf::Vertex vert;
-				vert.color = m_penColor;
-				m_grainShape.append(vert);
-			}
-			break;
-		case (TUBE_AND_ROD):
-			break;
-		case (STAR):
-			break;
-		case (DOUBLE_ANCHOR):
-			break;
-		case (WAGON_WHEEL):
-			break;
-		case (GRATE):
-			break;
-		case (DENDRITE):
-			break;
-		case (DOG_BONE):
-			break;
-		case (MULTIFIN):
-			break;
-		case (MULTIPERFORATED)
-			break;
-		}
-	}
+	m_grainCanvas->clear(sf::Color{m_BackgroundColor});
+	m_grainCanvas->draw(m_plot);
+	m_grainCanvas->display();
 }
-
-void GrainGraph::setPosition(sf::Vector2f position) { m_position = position; }
-
-void GrainGraph::setSize(sf::Vector2f size) { m_size = size; }
-
-void GrainGraph::(float width) { m_size.x = width; }
-
-void GrainGraph::setHeight(float height) { m_size.y = height; }
 
 void GrainGraph::setPenSize(int penSize) { m_penSize = penSize; }
 
-sf::Vector2f GrainGraph::getPosition() { return m_position; }
-
-sf::Vector2f GrainGraph::getSize() { return m_size; }
-
-float GrainGraph::getWidth() { return m_size.x;}
-
-float GrainGraph::getHeight() { return m_size.y; }
-
 int GrainGraph::getPenSize() { return m_penSize; }
 
-void GrainGraph::update() { draw(); }
+void GrainGraph::update() 
+{
+	m_plot.clearVertices();
+	m_plot.generateVertices(m_grain);
+	draw();
+}
+
+void GrainGraph::updateGrain(float grainRadius, float grainDepth, float innerRadius, float outerRadius, int numSpecializations, GrainGeometry geometry)
+{
+	m_grain.setGrainRadius(grainRadius);
+	m_grain.setGrainDepth(grainDepth);
+	m_grain.setGeometryRadiusInner(innerRadius);
+	m_grain.setGeometryRadiusOuter(outerRadius);
+	m_grain.setNumberOfSpecializations(numSpecializations);
+	m_grain.setGeometry(geometry);
+}
