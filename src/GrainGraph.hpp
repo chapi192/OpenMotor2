bf7107b422edbc0090bf63c3fce380e756fadef6
Elijah Roell <roell030@umn.edu>
2023-03-28 15:02:43 -0500

    Removed m_size, m_position, m_grainShape, getters and setters for position and size.Initialized m_visible to true and m_penSize to 1. Added m_grainShape, m_plot, and updateGrain. Moved m_grain into private. Changed parameters in ctor.

--- src/GrainGraph.hpp
+++ src/GrainGraph.hpp
@@ -1,44 +1,44 @@
 #pragma once
 #include <vector>
 #include "Grain.hpp"
+#include "PolarPlot.hpp"
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics.hpp>
+#include <TGUI/Widgets/ChildWindow.hpp>
+#include <TGUI/Widgets/TabContainer.hpp>
+#include <TGUI/Backend/Renderer/SFML-Graphics/CanvasSFML.hpp>
 
 class GrainGraph
 {
 public:
-	const sf::Vector2f center;
-	Grain grain;
 
-	GrainGraph();
-	GrainGraph(sf::Vector2f position, sf::Vector2f size);
+	//GrainGraph();
+	GrainGraph(tgui::Container::Ptr container, sf::Vector2f posInContainer, float length = 50);
 
 	~GrainGraph() = default;
-
+	 
 	void draw();
 
-	void setPosition(sf::Vector2f position);
-	void setSize(sf::Vector2f size);
-	void setWidth(float width);
-	void setHeight(float height);
 	void setPenSize(int penSize);
 
-	sf::Vector2f getPosition();
-	sf::Vector2f getSize();
-	float getWidth();
-	float getHeight();
 	int getPenSize();
 
 	void update();
 
+	void updateGrain(float grainRadius, float grainDepth, float innerRadius, float outerRadius, int numSpecializations, GrainGeometry geometry);
+
 private:
-	bool m_visable;
-	sf::Vector2f m_position;
-	sf::Vector2f m_size;
-	int m_penSize;
+	Grain m_grain;
+	PolarPlot m_plot;
+
+	tgui::CanvasSFML::Ptr m_grainCanvas;
+	
+	bool m_visible = true;
+	int m_penSize = 1;
+
+	sf::VertexArray m_grainShape;
+
 	sf::Color m_penColor;
 	sf::Color m_BackgroundColor;
-
-	sf::Vertex m_grainShape;
 };
 
