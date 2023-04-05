877d4fb3bbedb200d55e91d8955bf5947b0d6a86
Elijah Roell <roell030@umn.edu>
2023-04-04 22:44:33 -0500

    Implemented the two new ways of constructing a SeparatorLine.

--- src/SeparatorLineController.cpp
+++ src/SeparatorLineController.cpp
@@ -4,8 +4,30 @@ void SeparatorLineController::addSeparatorLine(sf::Vector2f position, sf::Vector
 {
 	auto newSeparatorLine = tgui::SeparatorLine::create();
 	newSeparatorLine->setPosition(position.x, position.y);
-	newSeparatorLine->setSize(dimensions.x, dimensions.y);
+	newSeparatorLine->setSize((tgui::Layout2d)dimensions);
 	newSeparatorLine->getRenderer()->setColor("Black");
+
+	m_separatorLines.emplace_back(newSeparatorLine);
+}
+
+void SeparatorLineController::addSeparatorLine(sf::Vector2f positionInContainer, sf::Vector2f dimensions, std::string name, tgui::Container::Ptr& container, tgui::Color color)
+{
+	auto newSeparatorLine = tgui::SeparatorLine::create();
+	newSeparatorLine->setPosition(positionInContainer.x, positionInContainer.y);
+	newSeparatorLine->setSize(dimensions.x, dimensions.y);
+	newSeparatorLine->getRenderer()->setColor(color);
+	container->add(newSeparatorLine);
+
+	m_separatorLines.emplace_back(newSeparatorLine);
+}
+
+void SeparatorLineController::addSeparatorLine(sf::Vector2f positionInGUI, sf::Vector2f dimensions, std::string name, tgui::BackendGui& gui, tgui::Color color)
+{
+	auto newSeparatorLine = tgui::SeparatorLine::create();
+	newSeparatorLine->setPosition(positionInGUI.x, positionInGUI.y);
+	newSeparatorLine->setSize(dimensions.x, dimensions.y);
+	newSeparatorLine->getRenderer()->setColor(color);
+	gui.add(newSeparatorLine);
 
 	m_separatorLines.emplace_back(newSeparatorLine);
 }
