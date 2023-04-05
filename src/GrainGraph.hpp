b90ee6e836fbe4ca023e71b96f09190ce8ae4bc5
Elijah Roell <roell030@umn.edu>
2023-04-04 22:41:27 -0500

    m_grain update methods now call GrainGraph's update. Added retrieval methods for all grain data members.

--- src/GrainGraph.hpp
+++ src/GrainGraph.hpp
@@ -25,23 +25,37 @@ public:
 
 	void update();
 
-	inline void updateGrainRadius(float grainRadius){ m_grain.setGrainRadius(grainRadius); 
+	float retrieveGrainRadius() { return m_grain.getGrainRadius(); }
+
+	float retrieveInnerGeometryRadius() { return m_grain.getGeometryRadiusInner(); }
+
+	float retrieveOuterGeometryRadius() { return m_grain.getGeometryRadiusOuter(); }
+
+	float retrieveGrainDepth() { return m_grain.getGrainDepth(); }
+
+	int retrieveNumSpecializations() { return m_grain.getNumberOfSpecializations(); }
+
+	std::string retrieveGrainGeometry();
+
+	inline void updateGrainRadius(float grainRadius) { m_grain.setGrainRadius(grainRadius); 
 													  update(); }
 
-	inline void updateGrainDepth(float grainDepth){ m_grain.setGrainDepth(grainDepth); 
+	inline void updateGrainDepth(float grainDepth) { m_grain.setGrainDepth(grainDepth); 
 												    update(); }
 	
-	inline void updateInnerGrainRadius(float innerRadius){ m_grain.setGeometryRadiusInner(innerRadius);
+	inline void updateInnerGrainRadius(float innerRadius) { m_grain.setGeometryRadiusInner(innerRadius);
 														   update(); }
 	
-	inline void updateOuterGrainRadius(float outerRadius){ m_grain.setGeometryRadiusOuter(outerRadius);
+	inline void updateOuterGrainRadius(float outerRadius) { m_grain.setGeometryRadiusOuter(outerRadius);
 														   update(); }
 	
-	inline void updateNumSpecializations(int numSpecializations){ m_grain.setNumberOfSpecializations(numSpecializations);
+	inline void updateNumSpecializations(int numSpecializations) { m_grain.setNumberOfSpecializations(numSpecializations);
 																  update(); }
 	
-	inline void updateGrainGeometry(GrainGeometry geometry){ m_grain.setGeometry(geometry);
+	inline void updateGrainGeometry(GrainGeometry geometry) { m_grain.setGeometry(geometry);
 															 update(); }
+
+	tgui::CanvasSFML::Ptr getCanvas() const { return m_grainCanvas; }
 
 private:
 	Grain m_grain;
