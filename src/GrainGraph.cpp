a01398ab7e01eaf1119c0122fffd73a2d1b62247
Elijah Roell <roell030@umn.edu>
2023-04-04 22:39:40 -0500

    m_plot initialization takes length now. Changed background color. Implemented retrieveGrainGeometry.

--- src/GrainGraph.cpp
+++ src/GrainGraph.cpp
@@ -5,7 +5,7 @@
 const float PI = 3.14159265;
 
 GrainGraph::GrainGraph(tgui::Container::Ptr container, sf::Vector2f posInContainer, float length) :
-	m_plot(container->getPosition(), m_grain),
+	m_plot(container->getPosition(), m_grain, length),
 	m_grainCanvas( tgui::CanvasSFML::create() )
 {
 	container->add(m_grainCanvas);
@@ -14,9 +14,47 @@ GrainGraph::GrainGraph(tgui::Container::Ptr container, sf::Vector2f posInContain
 	sf::Vector2f containerSize = container->getSize();
 	//float smallestLength = containerSize.x < containerSize.y ? containerSize.x : containerSize.y;
 	m_grainCanvas->setSize(length, length);
+	m_BackgroundColor = sf::Color{0xf0f0f0ff};
 }
 
-//GrainGraph::~GrainGraph() = default;
+std::string GrainGraph::retrieveGrainGeometry()
+{
+	std::string output = "";
+	switch(m_grain.getGrainGeometry()) {
+	case(DENDRITE):
+		output = "Dendrite";
+		break;
+	case(DOG_BONE):
+		output = "Dog Bone";
+		break;
+	case(DOUBLE_ANCHOR):
+		output = "Double Anchor";
+		break;
+	case(GRATE):
+		output = "Grate";
+		break;
+	case(MULTIFIN):
+		output = "Multifin";
+		break;
+	case(MULTIPERFORATED):
+		output = "Multiperforated";
+		break;
+	case(STAR):
+		output = "Star";
+		break;
+	case(TUBE):
+		output = "Tube";
+		break;
+	case(TUBE_AND_ROD):
+		output = "Tube and Rod";
+		break;
+	case(WAGON_WHEEL):
+		output = "Wagon Wheel";
+		break;
+	}
+
+	return output;
+}
 
 void GrainGraph::draw()
 {
@@ -32,11 +70,8 @@ int GrainGraph::getPenSize() { return m_penSize; }
 void GrainGraph::update() 
 {
 	m_plot.clearVertices();
-	std::cout << "Vertices were cleared" << std::endl;
 	m_plot.generateVertices(m_grain);
-	std::cout << "Vertices were generated" << std::endl;
 	draw();
-	std::cout << "Vertices were drawn" << std::endl;
 }
 
 
