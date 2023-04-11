5ac97eb3bbc8b521c1ec81417fcc94b64a33cf85
Elijah Roell <roell030@umn.edu>
2023-04-11 15:16:02 -0500

    Set default values for data, and added density and mass along with getters and setters.

--- src/Grain.hpp
+++ src/Grain.hpp
@@ -11,20 +11,28 @@ public:
 	void setGeometryRadiusInner(float newInnerRadius);
 	void setGeometryRadiusOuter(float newOuterRadius);
 	void setNumberOfSpecializations(int newNumSpecializations);
+	void setGrainDensity(float newGrainDensity);
+	void setGrainMass(float newGrainMass);
 	void setGeometry(GrainGeometry geometry);
 
 	float getGrainRadius();
 	float getGrainDepth();
 	float getGeometryRadiusInner();
 	float getGeometryRadiusOuter();
+	float getGrainDensity();
+	float getGrainMass();
 	int getNumberOfSpecializations();
 	GrainGeometry getGrainGeometry();
 
 private:
-	float m_grainRadius;
-	float m_grainDepth;
-	float m_geometryInnerRadius = 0;
-	float m_geometryOuterRadius;
-	int m_numberOfSpecializations; // the points on the star, the amount of bone ends, the amount of fins, etc.
+	float m_grainRadius = 0.5f;
+	float m_grainDepth = 0.5f;
+	float m_geometryInnerRadius = 0.f;
+	float m_geometryOuterRadius = 0.f;
+	int m_numberOfSpecializations = 0; // the points on the star, the amount of bone ends, the amount of fins, etc.
+
+	float m_grainDensity = 0.f;
+	float m_grainMass = 0.f;
+
 	GrainGeometry m_geometry = GrainGeometry::TUBE;
 };
